119783533dfb19b84e6535e95af36875b3c70c7a
Oliver Senduk <ollie.senduk@gmail.com>
2021-09-24 14:18:22 +0200

    add simple state with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState<number | null>(null);
+
+  const changeNumber = () => {
+    setNumber(10);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          {number}
         </p>
         <a
           className="App-link"
